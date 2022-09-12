d626a567ac799bf55060c3889b2e2555791b6fb1
20A91A0340 <106660236+20A91A0340@users.noreply.github.com>
2022-09-12 06:13:20 +0530

    import java.util.Scanner; class Main { 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int r; 		r=sc.nextInt(); 		for(int i=1;i<=2*r/2;i++) 		{ 		    int k=i>r?r-i%r:i; 			for(int j=1;j<=r;j++) 			{ 				if(j<=r-k) 					System.out.print(" "); 			} 			for(int p=1;p<=2*k-1;p++) 				System.out.print(i);
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-class Pattern
+class Main
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int N;
-        N=sc.nextInt();
-        
-        for(int i=1;i<=N;i++)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-        sc.close();
-    }
+	public static void main(String args[]) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int r;
+		r=sc.nextInt();
+		for(int i=1;i<=2*r/2;i++)			 
+		{  
+		    int k=i>r?r-i%r:i;
+			for(int j=1;j<=r;j++)	 
+			{
+				if(j<=r-k)
+					System.out.print(" ");
+			}
+			for(int p=1;p<=2*k-1;p++)
+				System.out.print(i);
+			
+			System.out.println();
+		}
+	}
 }
-            
