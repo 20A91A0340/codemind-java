6c2d0dad85c7d4f724d6677ec0cc3373f1f7b3dc
20A91A0340 <106660236+20A91A0340@users.noreply.github.com>
2022-09-13 21:55:25 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 if(i==j||i+j==n+1)                 System.out.print("x");                 else                 System.out.print("0");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -6,14 +6,17 @@ class Sample
         Scanner sc=new Scanner(System.in);
         int n;
         n=sc.nextInt();
-        for(int i=n;i<=n&&i>0;i--)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=n;j++)
             {
-                System.out.print(j+"");
+                if(i==j||i+j==n+1)
+                System.out.print("x");
+                else
+                System.out.print("0");
             }
             System.out.println();
-            
         }
     }
 }
+            
