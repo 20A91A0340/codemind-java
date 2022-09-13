07b795a4c5a9d3c8d9ca345c90f65120e98f0942
20A91A0340 <106660236+20A91A0340@users.noreply.github.com>
2022-09-13 23:13:17 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int P,T,R,s;         P=sc.nextInt();         T=sc.nextInt();         R=sc.nextInt();         s=(P*T*R)/100;         System.out.println(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int P,T,R,s;
+        P=sc.nextInt();
+        T=sc.nextInt();
+        R=sc.nextInt();
+        s=(P*T*R)/100;
+        System.out.println(s);
+    }
+}
