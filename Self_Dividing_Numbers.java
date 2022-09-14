b150c82e9d653d852e1bb2114fb2b9471341061f
20A91A0340 <106660236+20A91A0340@users.noreply.github.com>
2022-09-14 07:14:26 +0530

    import java.util.Scanner; class Sample {     public static boolean Self(int x)     {         int r,temp=x;         while(x>0)         {             r=x%10;             if(r==0)             return false;             if(temp%r!=0)             return false;             x=x/10;         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n1,n2;         n1=sc.nextInt();         n2=sc.nextInt();         for(int i=n1;i<=n2;i++)         {             if(Self(i))             System.out.print(i+" ");         }     } }

---
+++ Self_Dividing_Numbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Sample
+{
+    public static boolean Self(int x)
+    {
+        int r,temp=x;
+        while(x>0)
+        {
+            r=x%10;
+            if(r==0)
+            return false;
+            if(temp%r!=0)
+            return false;
+            x=x/10;
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1,n2;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        for(int i=n1;i<=n2;i++)
+        {
+            if(Self(i))
+            System.out.print(i+" ");
+        }
+    }
+}
+        
